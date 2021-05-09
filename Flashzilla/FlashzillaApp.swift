35e5176963fed9a6df696acd53222587158afed5
Lucas Parreira <59899994+lnparreira83@users.noreply.github.com>
2021-05-09 15:06:06 +1200

    Add files via upload

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Lucas Parreira on 23/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
